f1e87206663412e9115d50f972564355a863e32c
Mehereswar <mehereswar24@github>
2026-09-23 16:31:08 +0530

    fix: make dotenv optional + render.yaml build runs npm install

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+try { require('dotenv').config(); } catch (_) {}
 const express  = require('express');
 const cors     = require('cors');
 const morgan   = require('morgan');
